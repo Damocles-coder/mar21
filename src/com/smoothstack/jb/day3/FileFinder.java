8ac4902d6779027c618dd4f1fe905f87ffbe7630
--global <35568707+dyltra@users.noreply.github.com>
2021-03-11 09:49:23 -0800

    Made it so that the file finder class looks into sub-directories.

--- src/com/smoothstack/jb/day3/FileFinder.java
+++ src/com/smoothstack/jb/day3/FileFinder.java
@@ -18,6 +18,7 @@ public class FileFinder {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
+		FileFinder f = new FileFinder();
 		
 		//using the path resources/day3
 		System.out.print("Enter path you want to look up: ");
@@ -25,16 +26,29 @@ public class FileFinder {
 		System.out.println();
 		
 		File file = new File(path);
-		if (file.isDirectory()) {
-			String[] list = file.list();
-			for (String item:list) {
-				System.out.println(item);
-			}
+		if (file.isFile() || file.isDirectory()) {
+			f.printFolderContent(file, 0);
 		}
 		else {
-			System.out.println("Not a valid directory");
+			System.out.println("Not a folder.");
 		}
 		scanner.close();
+	}
+	
+	public void printFolderContent(File file, int level) {
+		File temp;
+		for (int i=0; i<level; i++) {
+			System.out.print("\t");
+		}
+		System.out.println(file.getName());
+		level++;
+		if (file.isDirectory()) {
+			File[] list = file.listFiles();
+			for (File item:list) {
+				temp = new File (file.getPath()+"/"+item);
+				printFolderContent(item, level);
+			}
+		}
 	}
 
 }
